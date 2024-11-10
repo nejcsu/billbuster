255870aebbb1e9ed4cbfd266d2e15fff414d756b
MUYUM <krasovec.n@gmail.com>
2024-11-10 18:00:15 +0100

    Added Swagger UI and documentation

--- src/supportScripts/connectorSIPX/Server.py
+++ src/supportScripts/connectorSIPX/Server.py
@@ -3,6 +3,9 @@ import json
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask import send_from_directory
+    
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from Main import ElecticityPrice
 
@@ -11,12 +14,24 @@ app = Flask(__name__)
 
 ep = ElecticityPrice();
 
+swaggerui_blueprint = get_swaggerui_blueprint(
+    '/API/DOCS',
+    '/DOCS/API.yaml',
+    config = { 'app_name': "Test application" }
+)
+
+app.register_blueprint(swaggerui_blueprint)
+
 @app.route("/")
 def Main():
     
     return "Connector SIPX"
 
-@app.route('/priceCatalog/<index>', methods=['GET'])
+@app.route('/DOCS/<path:path>')
+def send_report(path):
+    return send_from_directory('DOCS', path)
+    
+@app.route('/API/priceCatalog/<index>', methods=['GET'])
 def getPriceCatalog(index):
     
     res = { "Name": index.upper(), "Price": ep.GetPriceCatalogSIPX() }
